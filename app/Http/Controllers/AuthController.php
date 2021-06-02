63a311ed36e09586995398357e30430a6ab94945
PabloGustavoLinkNacional <maia.pablo@linknacional.com>
2021-06-02 15:42:16 -0300

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        return User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+    }
+
+    public function login(Request $request) {
+
+        if ( !Auth::attempt($request->only('email','password')) ){
+            return response([
+                'message' => 'Invalid credentials'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = Auth::user();
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        $cookie = cookie('jwt', $token, 60 * 24); // 60minutes  * 24 = 1day
+
+        return response([
+            'message' => 'successfully logged in'
+        ])->withCookie($cookie);
+
+    }
+
+    public function user() {
+        return Auth::user();
+    }
+
+    public function logout() {
+        $cookie = Cookie::forget('jwt');
+
+        return response([
+            'message' => 'successfully logged out'
+        ])->withCookie($cookie);
+    }
+}
